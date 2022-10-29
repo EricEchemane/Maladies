045e5ccb74ad191c515083933d4fb1ff4a2a198d
Eric Echemane <eechemane29@gmail.com>
2022-10-29 11:56:00 +0800

    results added

--- components/home/Results.tsx
+++ components/home/Results.tsx
@@ -14,18 +14,41 @@ const powdery = details["Squash Powdery Mildew"]
 export default function Results({ prediction }: Props) {
     return <>
         <Stack>
-            {/* {prediction.type === "Tomato Healthy" && <>
+            {prediction.type === "Tomato Healthy" && <>
                 <Title> Your Tomato Plant Is Healthy.Great Job! </Title>
-            </>} */}
-            {prediction.type !== "Tomato Septoria Leaf Spot" && <Stack>
+            </>}
+            {prediction.type === "Tomato Septoria Leaf Spot" && <Stack>
                 <Title> Tomato Septoria Leaf Spot </Title>
                 <Text> {septoria.descriptions.map(description => (
                     <p key={description}> {description} </p>
                 ))} </Text>
+                <Title order={3}> Treatments </Title>
+                <ul> {septoria.treatments.map(treatment => (
+                    <li key={treatment} style={{ marginBottom: '.5rem' }}> {treatment} </li>
+                ))} </ul>
+                <Title order={3}> Methods </Title>
+                <ul> {septoria.methods.map(method => (
+                    <li key={method} style={{ marginBottom: '.5rem' }}> {method} </li>
+                ))} </ul>
             </Stack>}
-            {/* {prediction.type === "Tomato Healthy" && <>
-                <Title> Your Tomato Plant Is Healthy.Great Job! </Title>
-            </>} */}
+            {prediction.type === "Squash Powdery Mildew" && <Stack>
+                <Title> Squash Powdery Mildew </Title>
+                <Text> {powdery.descriptions.map(description => (
+                    <p key={description}> {description} </p>
+                ))} </Text>
+                <Title order={3}> Treatments </Title>
+                <ul> {powdery.treatments.map(treatment => (
+                    <li key={treatment} style={{ marginBottom: '.5rem' }}> {treatment} </li>
+                ))} </ul>
+                <Title order={3}> Ingredients </Title>
+                <ul> {powdery.ingredients.map(ingredient => (
+                    <li key={ingredient} style={{ marginBottom: '.5rem' }}> {ingredient} </li>
+                ))} </ul>
+                <Title order={3}> Methods </Title>
+                <ul> {powdery.methods.map(method => (
+                    <li key={method} style={{ marginBottom: '.5rem' }}> {method} </li>
+                ))} </ul>
+            </Stack>}
         </Stack>
     </>
 }
